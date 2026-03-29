af5545232904a0dcc1d543dda23e0546c9e27872
Dragon-Knight <dubki4132@mail.ru>
2026-03-29 16:59:26 +0300

    fix ADC CHANNELs

--- include/OutputLogic.h
+++ include/OutputLogic.h
@@ -31,14 +31,14 @@ namespace Outputs
 	
 	inline void Setup()
 	{
-		outObj.AddPort( 0, {GPIOA, GPIO_PIN_1, ADC_CHANNEL_8}, 5000 );		// Выход 1
+		outObj.AddPort( 0, {GPIOA, GPIO_PIN_1, ADC_CHANNEL_1}, 5000 );		// Выход 1
 		outObj.AddPort( 1, {GPIOA, GPIO_PIN_2, ADC_CHANNEL_2}, 5000 );		// Выход 2
 		outObj.AddPort( 2, {GPIOA, GPIO_PIN_3, ADC_CHANNEL_3}, 5000 );		// Выход 3
 		outObj.AddPort( 3, {GPIOA, GPIO_PIN_4, ADC_CHANNEL_4}, 5000 );		// Выход 4
 		outObj.AddPort( 4, {GPIOA, GPIO_PIN_5, ADC_CHANNEL_5}, 5000 );		// Выход 5
 		outObj.AddPort( 5, {GPIOA, GPIO_PIN_6, ADC_CHANNEL_6}, 5000 );		// Выход 6
 		outObj.AddPort( 6, {GPIOA, GPIO_PIN_7, ADC_CHANNEL_7}, 15000 );		// Выход 7
-		outObj.AddPort( 7, {GPIOB, GPIO_PIN_0, ADC_CHANNEL_1}, 20000 );		// Выход HiPower-1
+		outObj.AddPort( 7, {GPIOB, GPIO_PIN_0, ADC_CHANNEL_8}, 20000 );		// Выход HiPower-1
 		
 		outObj.Init();
 
