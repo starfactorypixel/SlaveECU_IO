48e6d290e9655f55c5e3b6eb382092b94eb9abbe
Dragon-Knight <dubki4132@mail.ru>
2026-07-23 04:46:17 +0300

    Move ADC Regular logic to `AnalogRegular.h`

--- include/Analog.h
+++ include/Analog.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <AnalogRegular.h>
 #include <AnalogMux.h>
 #include <DrakePinA.hpp>
 #include <DrakePinD.hpp>
@@ -35,45 +36,6 @@ namespace Analog
 		PORT_IN9_NC,  PORT_IN10_NC, PORT_IN11_NC,  PORT_IN12_NC,
 		PORT_IN13_NC, PORT_IN14_NC, PORT_VIN,      PORT_NTC
 	};
-	
-	// Входные АЦП порты, обрабатываемые регулярной группой
-	enum port_regular_t : uint8_t
-	{
-		PORT_REG_NONE,
-		PORT_REG1, PORT_REG2, PORT_REG3, PORT_REG4, 
-		PORT_REG5, PORT_REG6, PORT_REG7, PORT_REG8
-	};
-
-
-
-	struct regular_channel_t
-	{
-		GPIO_TypeDef *port;
-		uint32_t pin;
-		uint32_t channel;
-		uint32_t rank;
-	};
-	
-	static constexpr regular_channel_t channels[] = 
-	{
-		{GPIOA, GPIO_PIN_1, ADC_CHANNEL_1, ADC_REGULAR_RANK_1},
-		{GPIOA, GPIO_PIN_2, ADC_CHANNEL_2, ADC_REGULAR_RANK_2},
-		{GPIOA, GPIO_PIN_3, ADC_CHANNEL_3, ADC_REGULAR_RANK_3},
-		{GPIOA, GPIO_PIN_4, ADC_CHANNEL_4, ADC_REGULAR_RANK_4},
-		{GPIOA, GPIO_PIN_5, ADC_CHANNEL_5, ADC_REGULAR_RANK_5},
-		{GPIOA, GPIO_PIN_6, ADC_CHANNEL_6, ADC_REGULAR_RANK_6},
-		{GPIOA, GPIO_PIN_7, ADC_CHANNEL_7, ADC_REGULAR_RANK_7},
-		{GPIOB, GPIO_PIN_0, ADC_CHANNEL_8, ADC_REGULAR_RANK_8}
-	};
-	static constexpr uint8_t regular_channel_count = sizeofarray(channels);
-	volatile uint16_t regular_buf[regular_channel_count];
-
-	const uint16_t GetRegularValue(/*port_regular_t*/ uint8_t num)
-	{
-		if(--num >= regular_channel_count) return 0;
-
-		return regular_buf[num];
-	}
 
 	const uint16_t GetMuxValue(port_mux_t port)
 	{
@@ -152,41 +114,11 @@ namespace Analog
 		
 		return;
 	}
-
-
-
-	static void RegularConfig()
-	{
-		GPIO_InitTypeDef GPIO_InitStruct = {0};
-		ADC_ChannelConfTypeDef sConfig = {0};
-
-		for(auto &channel : channels)
-		{
-			GPIO_InitStruct.Pin = channel.pin;
-			GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-			HAL_GPIO_Init(channel.port, &GPIO_InitStruct);
-
-			sConfig.Channel = channel.channel;
-			sConfig.Rank = channel.rank;
-			sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
-			if(HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-			{
-				Error_Handler();
-			}
-		}
-	}
-
-
-
-
-
-
-
+	
 	
 	inline void Setup()
 	{
-		HAL_ADCEx_Calibration_Start(&hadc1);
-		HAL_ADC_Start_DMA(&hadc1, (uint32_t *)regular_buf, regular_channel_count);
+		RegularSetup();
 
 		mux.Init();
 		adc_pin.Init();
